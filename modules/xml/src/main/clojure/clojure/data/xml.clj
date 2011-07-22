40d44310eb795158bf4cc3e9c71371de381ab700
Alan Malloy <alan@malloys.org>
2011-07-22 14:42:37 -0700

    Add a handler/parser for hiccup/prxml-style elements

--- modules/xml/src/main/clojure/clojure/data/xml.clj
+++ modules/xml/src/main/clojure/clojure/data/xml.clj
@@ -193,6 +193,66 @@
                   (InputSource. source)
                   source))))
 
+(defprotocol AsElements
+  (as-elements [expr] "Return a seq of elements represented by an expression."))
+
+(extend-protocol AsElements
+  clojure.lang.IPersistentVector
+  (as-elements [v]
+    (let [[tag & [attrs & after-attrs :as content]] v
+          [attrs content] (if (map? attrs)
+                            [(into {} (for [[k v] attrs]
+                                        [k (str v)]))
+                             after-attrs]
+                            [{} content])]
+      [(Element. tag attrs (mapcat as-elements content))]))
+
+  clojure.lang.ISeq
+  (as-elements [s]
+    (mapcat as-elements s))
+
+  clojure.lang.Keyword
+  (as-elements [k]
+    [(Element. k {} nil)])
+
+  java.lang.String
+  (as-elements [s]
+    [s])
+
+  nil
+  (as-elements [_] nil)
+
+  java.lang.Object
+  (as-elements [o]
+    [(str o)]))
+
+(defn sexps-as-fragment
+  "Convert a compact prxml/hiccup-style data structure into the more formal
+   tag/attrs/content format. A seq of elements will be returned, which may
+   not be suitable for immediate use as there is no root element. See also
+   sexp-as-element.
+
+   The format is [:tag-name attr-map? content*]. Each vector opens a new tag;
+   seqs do not open new tags, and are just used for inserting groups of elements
+   into the parent tag. A bare keyword not in a vector creates an empty element.
+
+   To provide XML conversion for your own data types, extend the AsElements
+   protocol to them."
+  ([] nil)
+  ([sexp] (as-elements sexp))
+  ([sexp & sexps] (apply concat (as-elements sexp)
+                         (map as-elements sexps))))
+
+(defn sexp-as-element
+  "Convert a single sexp into an Element"
+  [sexp]
+  (let [[root & more] (sexps-as-fragments sexp)]
+    (when more
+      (throw
+       (IllegalArgumentException.
+        "Cannot have multiple root elements; try creating a fragment instead")))
+    root))
+
 ;=== Emit-related functions ===
 
 (defn- attributes
