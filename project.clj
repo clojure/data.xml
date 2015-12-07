8efaea8137f8784d95759c3a42a0494e9783e233
Herwig Hochleitner <herwig@bendlas.net>
2015-12-07 16:33:14 +0100

    add project.clj for development with leiningen

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject org.clojure/data.xml "0-DEVELOPMENT"
+  :source-paths ["src/main/clojure"]
+  :test-paths ["src/test/clojure"]
+  :resource-paths ["src/test/resources"]
+  :dependencies [[org.clojure/clojure "1.8.0-RC3"]])
