68fcd51e2945aa35c6622f37287e0b3a557297db
Rafael de F. Ferreira <rafael@rafaelferreira.net>
2017-03-15 15:45:25 -0300

    Nubank forked version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojure/data.xml "0-UE-DEVELOPMENT"
+(defproject nu/data.xml "0.2.0-alpha3"
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure" "src/test/clojurescript"]
   :resource-paths ["src/test/resources" "target/gen-resources"]
@@ -10,4 +10,7 @@
                  [org.clojure/test.check "0.9.0"]
                  [figwheel-sidecar "0.5.8"]
                  [binaryage/devtools "0.8.3"]]
+  :deploy-repositories [["nu-maven" {:url        "s3p://nu-maven/releases/"
+                                     :sign-releases false }] ]
+
   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]})
