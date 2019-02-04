53f9eea41cf378ef5e4ee34476be0da01b44a4d5
Herwig Hochleitner <herwig@bendlas.net>
2019-02-05 00:31:47 +0100

    clean up and document PushHandler

--- src/main/clojure/clojure/data/xml/js/push.cljs
+++ src/main/clojure/clojure/data/xml/js/push.cljs
@@ -8,81 +8,6 @@
    [clojure.data.xml.tree :as tree]
    [clojure.data.xml.push-handler :as push-handler]))
 
-(defprotocol SaxHandler
-  (-open-tag [sh state name attributes])
-  (-close-tag [sh state name])
-  (-text [sh state string])
-  (-cdata [sh state string])
-  (-comment [sh state string])
-  (-end [sh state])
-  (-error [sh state error]))
-
-(defn unwrap-reduced [o]
-  (if (reduced? o)
-    @o o))
-
-(defn sh-wrapper [& {:keys [open-tag close-tag text cdata comment end error]}]
-  (fn [sh]
-    (reify SaxHandler
-      (-open-tag [_ state name attributes]
-        ((or open-tag -open-tag)
-         sh state name attributes))
-      (-close-tag [_ state name]
-        ((or close-tag -close-tag)
-         sh state name))
-      (-text [_ state string]
-        ((or text -text)
-         sh state string))
-      (-cdata [_ state string]
-        ((or cdata -cdata)
-         sh state string))
-      (-comment [_ state string]
-        ((or comment -comment)
-         sh state string))
-      (-end [_ state]
-        ((or end -end)
-         sh state))
-      (-error [_ state error]
-        ((or error -error)
-         sh state error)))))
-
-(defn event-xf-sh [xf]
-  (reify SaxHandler
-    (-open-tag [sh state name attributes]
-      (xf state {:type :start
-                 :name name
-                 :attributes attributes}))
-    (-close-tag [sh state name]
-      (xf state {:type :end
-                 :name name}))
-    (-text [sh state string]
-      (xf state {:type :chars
-                 :str string}))
-    (-cdata [sh state string]
-      (xf state {:type :cdata
-                 :str string}))
-    (-comment [sh state string]
-      (xf state {:type :comment
-                 :str string}))
-    (-end [sh state]
-      (xf state))
-    (-error [sh state error]
-      (let [s (xf state {:type :error
-                         :error error})]
-        (xf (unwrap-reduced s))))))
-
-(defn sh-event-xf [sh]
-  (fn
-    ([s] (-end sh (unwrap-reduced s)))
-    ([s {:keys [type name attributes str error]}]
-     (case type
-       :start (-open-tag sh s name attributes)
-       :end (-close-tag sh s name)
-       :chars (-text sh s str)
-       :cdata (-cdata sh s str)
-       :comment (-comment sh s str)
-       :error (-error sh s error)))))
-
 (defn qname [qn]
   (name/qname (gob/get qn "uri")
               (gob/get qn "local")
@@ -179,19 +104,15 @@
      "bar]]><a:gaga/><!--  la  la --></root"
      ">"]))
 
-  (let [p (parser
-           ((push-handler/ph-wrapper
-             :chars-event (fn [ph state string]
-                            (p/chars-event ph state (.toUpperCase string))))
-            tree/push-handler)
-           (list (transient []))
-           {})]
-    (p "<roo")
-    (p "t xmlns:a=\"GOO:\" foo=\"bar\" ")
-    (p "a:roo=\"ra\">lalala<![CDATA[foo GAARR ")
-    (p "bar]]><a:gaga/><!--  la  la --></root")
-    (p ">")
-    (p))
+  (-> (tree/event-tree
+       (sequence
+        (comp (parser-xf {})
+              push-handler/event-xf-ph)
+        ["<roo"
+         "t xmlns:a=\"GOO:\" foo=\"bar\" "
+         "a:roo=\"ra\">lalala<![CDATA[foo GAARR "
+         "bar]]><a:gaga/><!--  la  la --></root"
+         ">"])))
 
   )
 
--- src/main/clojure/clojure/data/xml/push_handler.cljc
+++ src/main/clojure/clojure/data/xml/push_handler.cljc
@@ -1,4 +1,8 @@
 (ns clojure.data.xml.push-handler
+  "A PushHandler can be translated to and from a Transducer, that would expect a push event.
+
+  It provides optimization opportunities, by eliminating intermediate Event allocations.
+  It does this, by unrolling push events into a protocol, in the fashion of SAX."
   (:require [clojure.data.xml.protocols :as p :refer [PushHandler push-events]]
             [clojure.data.xml.event :as event]
             [clojure.string :as str]
@@ -10,7 +14,9 @@
                                         core/split
                                         (core/map-vals (comp gensym str))
                                         p/push-methods)]
-   `(defn ~'ph-wrapper [& {:as ~overrides}]
+   `(defn ~'ph-wrapper
+      "Wrap PushHandler, overriding only specified methods"
+      [& {:as ~overrides}]
       (fn [~ph]
         (let [~@(eduction
                  (map first)
@@ -27,7 +33,9 @@
                             ~ph ~state ~@args))))
                   p/push-methods)))))))
 
-(defn event-xf-ph [xf]
+(defn event-xf-ph
+  "Get PushHandler from event-expecting transducer"
+  [xf]
   (code-gen
    [_ xf end-event] [state ph]
    `(reify PushHandler
@@ -41,7 +49,9 @@
       (~end-event [~_ ~state]
        (~xf ~state)))))
 
-(defn ph-event-xf [ph]
+(defn ph-event-xf
+  "Get event-expecting transducer from PushHandler"
+  [ph]
   (fn
     ([s] (p/end-event ph s))
     ([s event]
