11d6a9fd37b4ee322b21a9a39e7319b81e08e0fe
Alex Miller <alex.miller@cognitect.com>
2024-02-19 14:08:36 -0600

    update parent pom

--- project.clj
+++ project.clj
@@ -2,12 +2,15 @@
   :source-paths ["src/main/clojure" "src/main/clojurescript"]
   :test-paths ["src/test/clojure" "src/test/clojurescript"]
   :resource-paths ["src/main/resources" "src/test/resources" "target/gen-resources"]
-  :dependencies [[org.clojure/clojure "1.10.0-beta8"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/clojurescript "1.10.439"]
                  [cider/piggieback "0.5.3"]
                  [org.clojure/tools.nrepl "0.2.13"]
-                 [org.clojure/test.check "0.9.0"]
+                 [org.clojure/test.check "1.1.1"]
                  [figwheel-sidecar "0.5.17"]
                  [binaryage/devtools "0.9.10"]
                  [org.openjdk.nashorn/nashorn-core "15.3"]]
-  :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]})
+  :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}
+  :profiles {:dev {:dependencies [[com.github.nubank/morse "v2023.10.06.02"]]
+                   :repositories [["jitpack" "https://jitpack.io"]]}}
+  )
